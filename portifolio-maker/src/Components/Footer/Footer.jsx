3695bd2e87adc845a59bfd6e94a54ef5b40b22b4
Gustavo_lima07 <161834248+Gustavolima07@users.noreply.github.com>
2025-06-16 14:23:40 -0300

    Página inicial atualizada

--- portifolio-maker/src/Components/Footer/Footer.jsx
+++ portifolio-maker/src/Components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <footer className="items-center text-center bottom-0 lef-0 fixed w-full p-1
+        <footer className="items-center text-center bottom-0 lef-0 w-full p-1
         bg-black text-white text-2xl">
             <h1>Footer básico</h1>
         </footer>
--- portifolio-maker/src/Pages/Incial/Inicial.jsx
+++ portifolio-maker/src/Pages/Incial/Inicial.jsx
@@ -1,15 +1,32 @@
+import { Card } from '../../Components';
+
+import Img1 from '../../Images/Img1.png';
+
+
 const Inicial = () => {
+    
     return (
-        <div className="flex flex-col items-center justify-center ">
-        <h1 className="text-4xl font-bold mb-4">Bem-vindo ao Portfólio Maker!</h1>
-        <p className="text-lg text-gray-700 mb-8">Crie seu portfólio de forma fácil e rápida.</p>
-        <a
-            href="/portfolio"
-            className="px-6 py-3 bg-blue-600 text-white rounded hover:bg-blue-700 transition duration-300"
-        >
-            Começar
-        </a>
-        </div>
+        <>
+            <div className="App">
+                <div className="h-dvw">
+                    <Card tipo="inteiro" id="Home" className="mt-8 p-6 bg-rose-300  shadow-lg rounded-lg">
+                        <div className="flex items-center gap-8">
+                            <div className="Img flex-shrink-0">
+                                <img src={Img1} alt="Menino expressando felicidade" className="w-200 h-auto rounded-lg mb-4" />
+                            </div>
+                            <div className='text-center'>
+                                <h1 className="text-4xl font-bold ">Portfólio Maker</h1>
+                                <hr />
+                                <p className="text-2xl  text-gray-700 mb-4">
+                                    O Portfólio Maker é uma ferramenta inovadora que permite a criação de portfólios personalizados de forma rápida e intuitiva.
+                                    Com uma interface amigável, você pode adicionar suas informações, projetos e experiências profissionais em poucos cliques.
+                                </p>
+                            </div>
+                        </div>
+                    </Card>   
+                </div>
+            </div>
+        </>
     );
 }
 
